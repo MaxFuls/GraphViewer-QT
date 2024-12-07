72fb8b6c09e89689833328c715971edef2583039
MaxFuls <maxfuls2000@gmail.com>
2024-12-08 02:04:04 +0300

    fix graph generation

--- src/GraphBuilder/src/GraphBuilder.cpp
+++ src/GraphBuilder/src/GraphBuilder.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <utility>
+#include <algorithm>
 
 Graph GraphBuilder::build_random_graph(int vertex_count, int radius, int edges_min, int edges_max) const {
     std::vector<std::pair<int, int>> vertexes(vertex_count);
@@ -11,9 +12,13 @@ Graph GraphBuilder::build_random_graph(int vertex_count, int radius, int edges_m
     }
     for(int i = 0; i < vertex_count; ++i) {
         for(int j = 0, sub_vertex, edges_count = random.generate_on_range(edges_min, edges_max);
-                j < edges_count; ++j) {
+                j < edges_count && edges[i].size() < edges_count; ++j) {
             sub_vertex = random.generate_on_range(0, vertex_count - 1);
+            while(sub_vertex == i || std::find(edges[i].begin(), edges[i].end(), sub_vertex) != edges[i].end()) {
+                sub_vertex = random.generate_on_range(0, vertex_count - 1);
+            }
             edges[i].push_back(sub_vertex);
+            edges[sub_vertex].push_back(i);
         }
     }
     return Graph(vertexes, edges, radius);
