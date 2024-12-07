7ec908ca4004e67a9d597bbe93ad6ce331375a30
MaxFuls <maxfuls2000@gmail.com>
2024-12-08 01:54:32 +0300

    zoom and navigation added

--- src/GraphViewer/include/GraphViewer.hpp
+++ src/GraphViewer/include/GraphViewer.hpp
@@ -5,15 +5,25 @@
 
 #include <QMainWindow>
 #include <QtCore/QPointF>
+#include <QtGui/QMouseEvent>
+#include <QtGui/QWheelEvent>
 
 class GraphViewer : public QWidget {
     
     Graph graph_;
     double scale_ = 1.0;
-    QPointF cam_pos_ = {0.0, 0.0};
+    QPointF cam_pos_ = {0, 0};
+    bool mouse_pressed = false;
+    QPointF prev_mouse_pos;
+    QPointF elllipse_coords;
 public:
     GraphViewer(const Graph&, QWidget* parent = nullptr);
     void paintEvent(QPaintEvent*) override;
+    void mousePressEvent(QMouseEvent*) override;
+    void mouseReleaseEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
+    void resizeEvent(QResizeEvent*) override;
+    void wheelEvent(QWheelEvent*) override;
     ~GraphViewer();
 };
 
--- src/GraphViewer/src/GraphViewer.cpp
+++ src/GraphViewer/src/GraphViewer.cpp
@@ -3,37 +3,78 @@
 #include <QtWidgets/QWidget>
 #include <QtGui/QPainter>
 #include <QtGui/QColor>
-#include<iostream>
 
-GraphViewer::GraphViewer(const Graph& graph, QWidget* parent) : QWidget(parent), graph_(graph) {} 
+GraphViewer::GraphViewer(const Graph& graph, QWidget* parent) : QWidget(parent), graph_(graph) {
+    int min_side = std::min(width(), height());
+    double diag = graph_.get_radius() * 2;
+    scale_ = min_side / diag;
+    elllipse_coords = QPointF(0, 0);
+    repaint();
+} 
 
 GraphViewer::~GraphViewer() {}
 
-void GraphViewer::paintEvent(QPaintEvent* event) {
-    
-    double diam = 2 * graph_.get_radius();
-    int h = height();
-    int w = width();
-    int min_side = std::min(h, w);
-    double scale = min_side / diam;
+void GraphViewer::paintEvent(QPaintEvent*) {
+    int diam = graph_.get_radius() * 2;
+    int min_side = std::min(width(), height());
     QPainter painter(this);
     QColor color(0, 0, 0, 255);
     auto pen = QPen(color);
     painter.setPen(pen);
-    painter.drawEllipse(w/2 - min_side/2, h/2 - min_side/2, diam*scale, diam*scale);
+    painter.translate(-cam_pos_*scale_);
+    painter.drawEllipse(elllipse_coords*scale_, diam/2*scale_, diam/2*scale_);
     auto brush = QBrush(color);
     painter.setBrush(brush);
     auto vertxes = graph_.get_vertexes();
-    painter.translate(w/2, h/2);
     for(auto v :vertxes) {
-        painter.drawEllipse(QPoint(v.first, v.second)*scale, 5, 5);
+        painter.drawEllipse(QPoint(v.first, v.second)*scale_, 5, 5);
     }
     auto edges = graph_.get_edges();
     for(int i = 0, size = edges.size(); i < size; ++i) {
         for(int j = 0, sub_size = edges[i].size(); j < sub_size; ++j) {
-            painter.drawLine(QPoint(vertxes[i].first, vertxes[i].second)*scale,
-                             QPoint(vertxes[edges[i][j]].first, vertxes[edges[i][j]].second)*scale);
+            painter.drawLine(QPoint(vertxes[i].first, vertxes[i].second)*scale_,
+                             QPoint(vertxes[edges[i][j]].first, vertxes[edges[i][j]].second)*scale_);
         }
     }
 }
 
+void GraphViewer::mousePressEvent(QMouseEvent* event) {
+    mouse_pressed = true;
+    prev_mouse_pos = event->pos();
+}
+
+void GraphViewer::mouseReleaseEvent(QMouseEvent* event) {
+    mouse_pressed = false;
+}
+
+void GraphViewer::mouseMoveEvent(QMouseEvent* event) {
+    if(mouse_pressed) {
+        auto diff = event->pos() - prev_mouse_pos;
+        cam_pos_ -= diff/scale_;
+        prev_mouse_pos = event->pos();
+        repaint();
+    }
+}
+
+void GraphViewer::resizeEvent(QResizeEvent*) {
+    double diag = graph_.get_radius() * 2;
+    int min_side = std::min(width(), height());
+    scale_ = min_side / diag;
+    cam_pos_ = {-width()/(scale_*2), -height()/(scale_*2)};
+    elllipse_coords = QPointF(0, 0);
+    repaint();
+}
+
+void GraphViewer::wheelEvent(QWheelEvent* event) {
+    const double coef = 1.1;
+    double new_scale;
+    if(event->angleDelta().y() > 0) {
+        new_scale = scale_ * coef;
+    } else {
+        new_scale = scale_ / coef;
+    }
+    cam_pos_ += (event->position())*(1/scale_ - 1/new_scale);   
+    scale_ = new_scale;
+    prev_mouse_pos = event->position();
+    repaint();
+}
